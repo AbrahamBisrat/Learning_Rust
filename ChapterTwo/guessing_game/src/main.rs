5b3d92660ba5a7ac92d2d14792c75881fdb0392f
AbrahamMehari <abrahammehari0@gmail.com>
2023-07-11 23:45:20 -0500

    standard formatting - rustfmt main.rs

--- ChapterTwo/guessing_game/src/main.rs
+++ ChapterTwo/guessing_game/src/main.rs
@@ -1,39 +1,40 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Enter your guess");
- 
+
     let secret_num = rand::thread_rng().gen_range(1, 101);
     //println!("The random generated is {}: ", secret_num);
 
     //let mut guess = String::new();
 
     loop {
-	let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-	    .expect("Failed to read line");
-    
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
         let guess: u32 = match guess.trim().parse() {
-	    Ok(num) => num,
-	    Err(_) => {
-		println!("Wrong input, non-negative integers only. Eg. 1, 2, 3");
-		continue;
-	    },
-	};
+            Ok(num) => num,
+            Err(_) => {
+                println!("Wrong input, non-negative integers only. Eg. 1, 2, 3");
+                continue;
+            }
+        };
 
         println!("You guessed: {}", guess);
         //println!("The secret number is {}", secret_num);
-    
+
         match guess.cmp(&secret_num) {
             Ordering::Less => println!("Too Small!"),
-	    Ordering::Greater => println!("Too Big!"),
-	    Ordering::Equal => {
-		println!("Congrats You Won!");
-		break;
-	    }
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {
+                println!("Congrats You Won!");
+                break;
+            }
         }
     }
 }
